03c0501016c1fedc1583157c2202af27d2479fc5
zichuan3 <131895807+zichuan3@users.noreply.github.com>
2024-11-03 21:26:31 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require("core.lazy")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 -- 导入选项配置
-require("core.options")
+require("config.options")
 -- 导入键盘映射
-require("core.keymaps")
-require("core.autocmds")
+require("config.keymaps")
+-- 导入插件
+require("config.lazy")
+require("config.autocmds")
 -- 设置颜色
 -- evening zaibatsu wildcharm vim sorbet slate ron retrobox quiet murphy koehler industry habamax
