bb49294f136caae10c9ecb65db27339ef07417ce
zichuan3 <131895807+zichuan3@users.noreply.github.com>
2024-11-03 21:31:08 +0800

    Update options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,24 +1,45 @@
 local options = {
+	autowrite = true,
+	-- 是否自动重新加载已更改文件的选项
+	autoread = true,
 	-- 不创建备份文件
 	backup = false,
 	-- 允许neovim访问系统剪贴板
 	clipboard = "unnamedplus",
 	-- neovim 命令行中有更多空间用于显示消息
 	cmdheight = 2,
-	-- 当有单个补全项时显示补全菜单，有多个补全项时不自动选中第一个补全项
+	-- 显示补全菜单，不自动选中第一个补全项
 	completeopt = {"menu","menuone","noselect"},
-	-- 所有文本以原始形式显示
-	conceallevel = 0,
+	--隐藏粗体和斜体的*标记，但不隐藏替换标记
+	conceallevel = 2,
+	--退出修改后的缓冲区前确认保存更改
+	confirm = true,
+	-- 高亮当前行
+	cursorline = true,
+	-- Tab转换为空格
+	expandtab = true,
 	-- 编码
 	fileencoding = "UTF-8",
 	-- 高亮所有与上次搜索模式匹配的地方
 	hlsearch = true,
 	-- 搜索模式中忽略大小写
 	ignorecase = true,
+	-- 在跳转后恢复之前的视图状态
+	jumpoptions = "view",
+	-- 全局状态栏
+	laststatus = 3,
 	-- 启用鼠标
 	mouse = "a",
+	-- 控制缓冲区是否可修改
+	modifiable = true,
+	-- 显示行号
+	number = true,
+	-- 行号列的宽度
+	numberwidth = 4,
 	-- 弹出菜单高度
 	pumheight = 10,
+	-- 显示相对行号
+	relativenumber = false,
 	-- 始终显示标签页
 	showtabline = 2,
 	-- 智能区分大小写
@@ -30,42 +51,32 @@ local options = {
 	splitright = true,
 	-- 不创建交换文件
 	swapfile = false,
+	-- 总是显示标记栏
+	signcolumn = 'yes',
+	-- 不显示模式，因为我们有状态栏
+	showmode = false,
+	-- 滚动偏移量
+	scrolloff = 8,
+	-- 侧边滚动偏移量
+	sidescrolloff = 8,
+	-- 自动缩进时插入的空格数
+	shiftwidth = 2,
+	-- 插入制表符时实际插入的空格数量。
+	softtabstop = 2,
 	-- 设置终端GUI颜色
 	termguicolors = true,
 	-- 映射序列完成前等待的时间
 	timeoutlen = 1000,
+	-- Tab宽度
+	tabstop = 2,
 	-- 启动持久撤销
 	undofile = true,
 	-- 更快的检测文件变化
 	updatetime = 1000,
 	-- 不创建写入备份文件
 	writebackup = false,
-	-- Tab转换为空格
-	expandtab = true,
-	-- 缩进时插入的空格数
-	shiftwidth = 2,
-	-- Tab宽度
-	tabstop = 2,
-	-- 高亮当前行
-	cursorline = true,
-	-- 显示行号
-	number = true,
-	-- 显示相对行号
-	relativenumber = false,
-	-- 行号列的宽度
-	numberwidth = 4,
-	-- 总是显示标记栏
-	signcolumn = 'yes',
 	-- 不会自动换行
 	wrap = false,
-	-- 滚动偏移量
-	scrolloff = 8,
-	-- 侧边滚动偏移量
-	sidescrolloff = 8,
-	-- gui应用中的字体
-	guifont = 'monospace:h17',
-	-- 确保缓冲区的modifiable打开
-	modifiable = true
 }
 -- 不显示命令结果
 vim.opt.shortmess:append('c')
